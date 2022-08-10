550c2ac340ca366ce6a473e6aa5c287902dedfeb
24juice22 <mike.james.nunn@gmail.com>
2022-08-10 09:34:25 -0400

    Nav-Link Click Hides Nav
    Clicking on a navigation link will hide the dropdown Nav. Also added and styled a button to see resume on the about page.

--- toggler.js
+++ toggler.js
@@ -2,12 +2,24 @@ let toggler = document.querySelector(".toggler");
 let nav = document.querySelector(".navbar-nav");
 let menu = document.querySelector(".toggler__menu");
 let closed = document.querySelector(".toggler__closed");
+let link = document.querySelectorAll(".nav-link");
 
 toggler.addEventListener("click", () => {
     nav.classList.toggle("active")
     closed.classList.toggle("show")
     menu.classList.toggle("hide")
 })
+
+for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener("click", () => {
+        nav.classList.toggle("active")
+        closed.classList.toggle("show")
+        menu.classList.toggle("hide")
+    })
+}
+
+
+
 
 
 
