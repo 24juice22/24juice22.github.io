c70ce5f92d1c21118c697cc1eb029d70b6e4cc59
24juice22 <mike.james.nunn@gmail.com>
2022-08-12 00:44:45 -0400

    New Navbar and Collapsible
    Navbar will always have toggler present (even on large screens). The original "navbar__list" will no longer collapse down when the toggler is clicked. A new fullscreen collapsible-menu will appear instead. Some functionality still needs to be coded such as the links for the collapsible-menu.

--- toggler.js
+++ toggler.js
@@ -3,19 +3,32 @@ let nav = document.querySelector(".navbar__list");
 let menu = document.querySelector(".toggler__menu");
 let closed = document.querySelector(".toggler__closed");
 let link = document.querySelectorAll(".navbar__link");
+let hero = document.querySelector(".hero__content");
+let logo = document.querySelector(".navbar__logo");
+let collapsibleMenu = document.querySelector(".collapsible-menu");
+
+
+console.log(logo.style);
+////// adjust Toggler
 
 toggler.addEventListener("click", () => {
-    nav.classList.toggle("active")
+    collapsibleMenu.classList.toggle("good");
     closed.classList.toggle("show")
     menu.classList.toggle("hide")
+    logo.classList.toggle("white");
+    toggler.classList.toggle("white")
 })
 
+/////// Hide Nav-List when Link is Clicked
+
 for (let i = 0; i < link.length; i++) {
-    link[i].addEventListener("click", () => {
-        nav.classList.toggle("active")
-        closed.classList.toggle("show")
-        menu.classList.toggle("hide")
-    })
+    link[i].addEventListener("click", hideNavList);
+}
+
+function hideNavList() {
+    nav.classList.toggle("active");
+    closed.classList.toggle("show");
+    menu.classList.toggle("hide");
 }
 
 
