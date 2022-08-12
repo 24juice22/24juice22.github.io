cd6d9504dab376c36ec598628f878abee0496f18
24juice22 <mike.james.nunn@gmail.com>
2022-08-12 10:11:30 -0400

    On Scroll Navigation
    On Scroll hide "navbar__list" and change color of logo and toggler when they reach the "about__attributes" section

--- toggler.js
+++ toggler.js
@@ -6,7 +6,7 @@ let link = document.querySelectorAll(".collapsible-menu__link");
 let hero = document.querySelector(".hero__content");
 let logo = document.querySelector(".navbar__logo");
 let collapsibleMenu = document.querySelector(".collapsible-menu");
-
+let about = document.querySelector(".about__attributes");
 
 console.log(logo.style);
 ////// adjust Toggler
@@ -33,8 +33,47 @@ function hideNavList() {
     toggler.classList.toggle("white")
 }
 
+/////// Debounce Function ///////
+function debounce(func, wait = 15, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 
+//////// Hide nav-list on scroll //////////////
+function checkNavScroll(e) {
+    const logoBottom = (window.scrollY + logo.offsetHeight);
+    const heroTop = hero.offsetTop;
+    if (logoBottom > heroTop) 
+        nav.classList.add("hidden");
+    else
+        nav.classList.remove("hidden");
+}
 
+window.addEventListener("scroll", debounce(checkNavScroll))
 
+//////// Change Logo/Toggler colors on scroll /////////////////////
+function logoColor(e) {
+    const logoBottom = (window.scrollY + logo.offsetHeight);
+    const aboutTop = about.offsetTop;
+    if (logoBottom > aboutTop) {
+        logo.classList.add("white");
+        toggler.classList.add("white");
+    }
+    else {
+        logo.classList.remove("white");
+        toggler.classList.remove("white");
+    }
+}
 
+window.addEventListener("scroll", debounce(logoColor))
 
