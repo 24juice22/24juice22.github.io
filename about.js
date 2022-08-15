81d7a1f2ee18380956b577da8465ac77c72a22a9
24juice22 <mike.james.nunn@gmail.com>
2022-08-15 18:58:21 -0400

    Redid Attribute Visible On Scroll
    Redid the code for making the attributes visible on scroll. Original code did not work on actual phone.

--- about.js
+++ about.js
@@ -16,17 +16,33 @@ function debounce(func, wait = 15, immediate = true) {
 
 //////// Attribute Becomes Visible on Scroll //////////////
 let attribute = document.querySelectorAll(".about__attribute");
+let attribute1 = document.querySelector(".about__attribute-1");
+let attribute2 = document.querySelector(".about__attribute-2");
+let attribute3 = document.querySelector(".about__attribute-3");
+let attribute4 = document.querySelector(".about__attribute-4");
 let attributeParent = attribute[0].offsetParent;
-let attributeParentParent = attributeParent.offsetParent
-for (let i = 0; i < attribute.length; i++) {
-    function attributeVisible(e) {
-        const windowBottom = (window.scrollY + window.innerHeight);
-        const attributeVisibleLevel = attributeParentParent.offsetTop + attributeParent.offsetTop + attribute[i].offsetTop + (attribute[i].offsetHeight / 1.2);
-        if (windowBottom > attributeVisibleLevel) 
-            attribute[i].classList.add("visible")
-    }
-    window.addEventListener("scroll", debounce(attributeVisible));
+let attributeParentParent = attributeParent.offsetParent;
+
+function attributeVisible(e) {
+    const windowBottom = (window.scrollY + window.innerHeight);
+    const attribute1VisibleLevel = attributeParentParent.offsetTop + attributeParent.offsetTop + attribute1.offsetTop + (attribute1.offsetHeight / 1.2);
+    const attribute2VisibleLevel = attributeParentParent.offsetTop + attributeParent.offsetTop + attribute2.offsetTop + (attribute2.offsetHeight / 1.2);
+    const attribute3VisibleLevel = attributeParentParent.offsetTop + attributeParent.offsetTop + attribute3.offsetTop + (attribute3.offsetHeight / 1.2);
+    const attribute4VisibleLevel = attributeParentParent.offsetTop + attributeParent.offsetTop + attribute4.offsetTop + (attribute4.offsetHeight / 1.2);
+    console.log(windowBottom);
+    console.log("att1:   " + attribute1VisibleLevel)
+    if (windowBottom > attribute1VisibleLevel) 
+        attribute1.classList.add("visible");
+    if (windowBottom > attribute2VisibleLevel) 
+        attribute2.classList.add("visible"); 
+    if (windowBottom > attribute3VisibleLevel) 
+        attribute3.classList.add("visible");
+    if (windowBottom > attribute4VisibleLevel) 
+        attribute4.classList.add("visible");
 }
+
+window.addEventListener("scroll", debounce(attributeVisible));
+
 
  
 
